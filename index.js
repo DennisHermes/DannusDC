ae85f16aba2921d1be5082e9746582f380e4d072
Dennis Hermes <dennishermes2004@gmail.com>
2024-05-16 23:00:42 +0200

    init

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 const { Client, Intents } = require('discord.js');
 const { token } = require('./config.json');
+const webhook = require('./webhook');
 
 const client = new Client({ intents : [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES, Intents.FLAGS.GUILD_VOICE_STATES, Intents.FLAGS.DIRECT_MESSAGES] , partials : ['CHANNEL', 'MESSAGE']})
-
 
 //on ready
 client.once('ready', () => {
 	console.log('[Dannus] Done loading!');
+
+	webhook.longPoll(client);
 });
 
 //Login to discord
---
+++ webhook.js
@@ -0,0 +1,53 @@
+const axios = require('axios');
+const { MessageEmbed } = require("discord.js")
+
+function longPoll(client) {
+    let checkpoint = new Date();
+
+    const fetchData = async () => {
+        try {
+            const response = await axios.get('https://dannus.net/api/plugins/receive');
+            return response.data;
+        } catch (error) {
+            console.error('Error fetching data:', error);
+            throw error;
+        }
+    }
+
+    const poll = async () => {
+        try {
+            const data = await fetchData();
+            const messageDate = new Date(data.date);
+            if (messageDate > checkpoint) {
+                checkpoint = messageDate;
+                
+                const channel = client.channels.cache.get(data.discord_channel);
+                if (!channel) {
+                    console.error('Channel not found');
+                    return;
+                }
+
+                const embed = new MessageEmbed().setTitle("New Notification");
+                const fields = new Array();
+                for(var obj in data) {
+                    if (obj == "date" || obj == "discord_channel") continue;
+                    fields.push({name: obj, value: data[obj] + "     ", inline: true});
+                }
+                embed.setFields(fields);
+                embed.setTimestamp(data.date);
+                embed.setColor("#50bc14");
+                embed.setThumbnail("https://dannus.net/media/logo.png");
+                channel.send({ embeds: [embed] });
+
+            }
+        } catch (error) {
+            console.error('Error during polling:', error);
+        }
+    };
+
+    setInterval(poll, 1000);
+}
+
+module.exports = {
+    longPoll: longPoll
+};
